0c9597d9cc934051acb6967ed710187b9d8eecdf
Diego Alex Rossi Gradini <diegorossi8@gmail.com>
2022-01-28 08:54:54 -0300

    Create EnvioWhats.py
    Para utilização deste deve se criar um arquivo no excel com os seguinte:
    
    Coluna A                Coluna B                      Coluna C
    Pessoa                    Número                        Mensagem
    
    O número deve seguir o padrão DDI + DD+ Número ex: 5511999999999
    
    ATT

---
+++ EnvioWhats.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+contatos_df = pd.read_excel("DIRETORIO DO SEU ARQUIVO XLSX")
+display(contatos_df)
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib
+
+navegador = webdriver.Chrome()
+
+# já estamos com o login feito no whatsapp web
+for i, mensagem in enumerate(contatos_df['Mensagem']):
+    pessoa = contatos_df.loc[i, "Pessoa"]
+    numero = contatos_df.loc[i, "Número"]
+    texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem}")
+    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+    navegador.get(link)
+    while len(navegador.find_elements_by_id("side")) < 1:
+        time.sleep(10)
+    navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
+    time.sleep(10)

85e3ac0838f8cb0b6f2e8b1543a49ceadbb6e1ab
Diego Alex Rossi Gradini <diegorossi8@gmail.com>
2022-01-28 15:55:08 -0300

    Update Envio Email.py
    Correções após teste de 100 0 envios!

--- Envio Email.py
+++ Envio Email.py
@@ -10,24 +10,30 @@ import smtplib
 
 
 # Criando objeto
-server = smtplib.SMTP('smtp.gmail.com: 587')
-# Login com servidor
-server.ehlo()
-server.starttls()
-server.login('seuemail@gmail.com', 'suasenha')
+
         
 for i, mensagem in enumerate(contatos_df['Mensagem']):
-    pessoa = contatos_df.loc[i, "Pessoa"]
-    email = contatos_df.loc[i, "Email"]
-    texto = (f"Olá, {pessoa}!\n{mensagem}")
+    try:
+        server = smtplib.SMTP('smtp.gmail.com: 587')
+        # Login com servidor
+        server.ehlo()
+        server.starttls()
+        server.login('EMAIL@gmail.com', 'SENHA')
+        pessoa = contatos_df.loc[i, "Pessoa"]
+        email = contatos_df.loc[i, "Email"]
+        texto = (f"Olá, {pessoa}!\nTudo bem? \n A CONQUER liberou gratuitamente o curso de Produtividade e Gestão do Tempo.\nAprenda estratégias e ferramentas de produtividade para gerir melhor o seu tempo, priorizar seus sonhos e alcançar os objetivos desejados. \nCurso Gratuito Produtividade e Gestão do Tempo | Escola Conquer\nhttps://upvir.al/ref/61f425eff1d88M")
 
-    # Criando mensagem
-    message = texto
-    email_msg = MIMEMultipart()
-    email_msg['From'] = user
-    email_msg['To'] = email
-    email_msg['Subject'] = "IMPORTANTE !!!"
-    email_msg.attach(MIMEText(message, 'plain'))
+        # Criando mensagem
+        message = texto
+        email_msg = MIMEMultipart()
+        email_msg['From'] = "EMAIL@gmail.com"
+        email_msg['To'] = email
+        email_msg['Subject'] = "IMPORTANTE !!! CURSO GRATUITO"
+        email_msg.attach(MIMEText(message, 'plain'))
         # Enviando mensagem
-    server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
-    print(f'Mensagem {i} enviada!')
+        server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+        print(f'Mensagem {i} enviada!')
+        server.close()
+    except:
+        print(f'Mensagem {i} NÃO!')
+        pass
